7dce3e8c37554633c20c4e1626fd7c87115414e9
black.zzj <black.zzj@alibaba-inc.com>
2018-05-22 17:56:26 +0800

    添加位置同步和碰撞同步

---
+++ workers/unity/Assets/Gamelogic/Movement/CapsuleColliderReceiver.cs
@@ -0,0 +1,112 @@
+﻿using Improbable.Movement;
+using Improbable.Unity.Visualizer;
+using Improbable.Worker;
+using UnityEngine;
+
+namespace Gamelogic.Movement
+{
+	public class CapsuleColliderReceiver : MonoBehaviour
+	{
+		[Require] private CapsuleColliderData.Reader mCapsuleColliderDataReader;
+
+		private CapsuleCollider mCollider;
+		private void OnEnable()
+		{
+			mCollider = GetComponent<CapsuleCollider>();
+			if(mCapsuleColliderDataReader == null)
+				return;
+			
+			mCapsuleColliderDataReader.CenterXUpdated.Add(OnCenterXReader);
+			mCapsuleColliderDataReader.CenterYUpdated.Add(OnCenterYReader);
+			mCapsuleColliderDataReader.CenterZUpdated.Add(OnCenterZReader);
+			mCapsuleColliderDataReader.RadiusUpdated.Add(OnRadiusReader);
+			mCapsuleColliderDataReader.HeightUpdated.Add(OnHeightReader);
+			mCapsuleColliderDataReader.TriggerUpdated.Add(OnIsTriggerReader);
+		}
+
+		private void OnDisable()
+		{
+			if(mCapsuleColliderDataReader == null)
+				return;
+			
+			mCapsuleColliderDataReader.CenterXUpdated.Add(OnCenterXReader);
+			mCapsuleColliderDataReader.CenterYUpdated.Add(OnCenterYReader);
+			mCapsuleColliderDataReader.CenterZUpdated.Add(OnCenterZReader);
+			mCapsuleColliderDataReader.RadiusUpdated.Add(OnRadiusReader);
+			mCapsuleColliderDataReader.HeightUpdated.Add(OnHeightReader);
+			mCapsuleColliderDataReader.TriggerUpdated.Add(OnIsTriggerReader);
+		}
+
+		private void OnCenterXReader(float value)
+		{
+			if(mCapsuleColliderDataReader == null || mCapsuleColliderDataReader.Authority == Authority.NotAuthoritative)
+				return;
+			
+			if(mCollider == null)
+				return;
+
+			Vector3 center = mCollider.center;
+			center.x = value;
+			mCollider.center = center;
+		}
+		
+		private void OnCenterYReader(float value)
+		{
+			if(mCapsuleColliderDataReader == null || mCapsuleColliderDataReader.Authority == Authority.NotAuthoritative)
+				return;
+
+			if(mCollider == null)
+				return;
+
+			Vector3 center = mCollider.center;
+			center.y = value;
+			mCollider.center = center;
+		}
+		
+		private void OnCenterZReader(float value)
+		{
+			if(mCapsuleColliderDataReader == null || mCapsuleColliderDataReader.Authority == Authority.NotAuthoritative)
+				return;
+
+			if(mCollider == null)
+				return;
+
+			Vector3 center = mCollider.center;
+			center.z = value;
+			mCollider.center = center;
+		}
+
+		private void OnRadiusReader(float value)
+		{
+			if(mCapsuleColliderDataReader == null || mCapsuleColliderDataReader.Authority == Authority.NotAuthoritative)
+				return;
+
+			if(mCollider == null)
+				return;
+
+			mCollider.radius = value;
+		}
+		
+		private void OnHeightReader(float value)
+		{
+			if(mCapsuleColliderDataReader == null || mCapsuleColliderDataReader.Authority == Authority.NotAuthoritative)
+				return;
+
+			if(mCollider == null)
+				return;
+
+			mCollider.height = value;
+		}
+		
+		private void OnIsTriggerReader(bool value)
+		{
+			if(mCapsuleColliderDataReader == null || mCapsuleColliderDataReader.Authority == Authority.NotAuthoritative)
+				return;
+
+			if(mCollider == null)
+				return;
+
+			mCollider.isTrigger = value;
+		}
+	}
+}
---
+++ workers/unity/Assets/Gamelogic/Movement/TransformReceiver.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Improbable;
+using Improbable.Core;
+using UnityEngine;
+using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using Improbable.Worker;
+
+namespace Gamelogic.Movement
+{
+	public class TransformReceiver : MonoBehaviour
+	{
+		[Require] private Position.Reader mPositionReader;
+
+		[Require] private Rotation.Reader mRotationReader;
+		// Use this for initialization
+		private void OnEnable()
+		{
+			if (mPositionReader != null)
+				mPositionReader.CoordsUpdated.Add(ReceivePositionSync);
+			
+			if(mRotationReader != null)
+				mRotationReader.RotationUpdated.Add(ReceiveRotationSync);
+		}
+
+		private void OnDisable()
+		{
+			if (mPositionReader != null)
+				mPositionReader.CoordsUpdated.Remove(ReceivePositionSync);
+			
+			if(mRotationReader != null)
+				mRotationReader.RotationUpdated.Remove(ReceiveRotationSync);
+		}
+
+		public void ReceivePositionSync(Coordinates coor)
+		{
+			if (mPositionReader.Authority == Authority.NotAuthoritative)
+				transform.position = coor.ToUnityVector();
+		}
+
+		public void ReceiveRotationSync(float eulerY)
+		{
+			if (mRotationReader.Authority == Authority.NotAuthoritative)
+				transform.rotation = Quaternion.Euler(0, eulerY, 0);
+		}
+	}
+}
